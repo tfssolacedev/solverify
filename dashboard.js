123e4a2e0806af59e000137cf145074c611256a9
Solace <monkdev9@gmail.com>
2025-05-07 23:41:01 +0100

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-const storedUser = localStorage.getItem("discordUser");
-if (!storedUser) {
-  window.location.href = "/";
-} else {
-  const user = JSON.parse(storedUser);
-  document.getElementById("username").textContent = `${user.username}#${user.discriminator}`;
-  document.getElementById("avatar").src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
-}
